08f05f4f4249e3db10c84218a9cc1ff1311373d9
elgatito31 <104541053+elgatito31@users.noreply.github.com>
2023-10-13 21:54:03 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,4 +1,36 @@
 import requests
+from bs4 import BeautifulSoup
+
+def extract_source(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
+    source = requests.get(url, headers=headers).text
+    return source
+
+buffalo_bills_roster_url = "https://www.espn.com/nfl/team/roster/_/name/buf/buffalo-bills"
+page = extract_source(buffalo_bills_roster_url)
+soup = BeautifulSoup(page, 'html.parser')
+
+table_bodies = soup.select('.Table__TBODY')
+
+players_details = []
+
+for b in table_bodies:
+    rows = b.select(".Table__TR.Table__TR--lg.Table__even")
+    for row in rows:
+        columns = row.select(".Table__TD")
+        rawName = columns[1].text
+        name = "".join([c for c in rawName if not c.isdigit()])
+        position = columns[2].text
+        age = columns[3].text
+        height = columns[4].text
+
+        # Store the details in the list
+        players_details.append(f"{name} {position} {age} {height}")
+
+#Run it like this to see the printed playas, but not necessary :
+for player_info in players_details:
+    print(player_info)
+'''import requests
 
 from bs4 import BeautifulSoup
 from database import *
@@ -35,4 +67,4 @@ for b in table_bodies:
         age = columns[3].text
         height = columns[4].text
 
-        print(f"{name} {position} {age} {height}")
+        print(f"{name} {position} {age} {height}")'''
